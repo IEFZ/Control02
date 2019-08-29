9509c54ee81f8c8d6d6589b270197b9e49201f3c
xlChris <cristobal.moralesm@outlook.com>
2019-08-28 21:49:54 -0400

    se separaron las funciones del main

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -5,12 +5,66 @@
 using namespace std;
 using namespace cv;
 
-int main(int argc, char const *argv[]) {
+bool validar_argumentos(string arg_resolucion, string arg_entrada, string arg_salida){
+    if (arg_resolucion.compare("")==0){
+        cout << "ATENCION:Falta argumento -e y/o la resolucion " << endl;
+        return false;
+    }
+    if (arg_entrada.compare("")==0){
+        cout << "ATENCION:Falta argumento -f y/o la ruta del arhivo entrada " << endl;
+        return false;
+    }
+    if (arg_salida.compare("")==0){
+        cout << "ATENCION:Falta argumento -o y/o la ruta del arhivo salida " << endl;
+        return false;
+    }
+    return true;
+}
+
+void crear_nueva_imagen(string arg_resolucion,string arg_salida, cv::Mat image){
+    cv::Mat imagen_resize;
+    bool resolucion_correcta = true;
+    if (arg_resolucion.compare("FHD") == 0 || arg_resolucion.compare("fhd") == 0){
+        cv::resize(image,imagen_resize,cv::Size(1920,1080));
+        cv::imwrite(arg_salida,imagen_resize);
+        resolucion_correcta = false;
+    }
+    if (arg_resolucion.compare("2K") == 0 || arg_resolucion.compare("2k") == 0){
+        cv::resize(image,imagen_resize,cv::Size(2048,1080));
+        cv::imwrite(arg_salida,imagen_resize);
+        resolucion_correcta = false;
+    }
+    if (arg_resolucion.compare("4K") == 0 || arg_resolucion.compare("4k") == 0){
+        cv::resize(image,imagen_resize,cv::Size(4096,2160));
+        cv::imwrite(arg_salida,imagen_resize);
+        resolucion_correcta = false;
+    }
+    if (arg_resolucion.compare("8K") == 0 || arg_resolucion.compare("8k") == 0){
+        cv::resize(image,imagen_resize,cv::Size(7680,4320));
+        cv::imwrite(arg_salida,imagen_resize);
+        resolucion_correcta = false;
+    }
+    if (resolucion_correcta){
+        cout << "ATENCION: Especifique correctamente la resolucion (FHD, 2K, 4K, 8K)" << endl;
+    }
+}
+
+int leer_imagen(string arg_entrada,string arg_resolucion,string arg_salida){
+    cv::Mat image = cv::imread(arg_entrada, CV_LOAD_IMAGE_COLOR);
+    if(!image.data) {
+        std::cout << "Error: no se cargo la imagen." << std::endl;
+        return -1;
+    }
+    crear_nueva_imagen(arg_resolucion,arg_salida,image);
+}
+
+void asociar_argumentos_imagen(int argc, char const *argv[]){
     string arg_resolucion;
     string arg_entrada;
     string arg_salida;
+    string dato;
     for (int i=1; i<argc; i=i+2){
-        string dato=argv[i];
+        dato=argv[i];
         if (dato.compare("-e") == 0){
             if (!(i==(argc-1))){
                 arg_resolucion=argv[i+1];
@@ -27,41 +81,13 @@ int main(int argc, char const *argv[]) {
             }
         }
     }
-    if (arg_resolucion.compare("")==0){
-        cout << "Falta argumento -e y/o la resolucion " << endl;
+    if (validar_argumentos(arg_resolucion,arg_entrada,arg_salida)){
+        leer_imagen(arg_entrada,arg_resolucion,arg_salida);
     }
-    if (arg_entrada.compare("")==0){
-        cout << "Falta argumento -f y/o la ruta del arhivo entrada " << endl;
-    }
-    if (arg_salida.compare("")==0){
-        cout << "Falta argumento -o y/o la ruta del arhivo salida " << endl;
-    }
-    cv::Mat image = cv::imread(arg_entrada, CV_LOAD_IMAGE_COLOR);
-	cv::Mat dst;
-    if(!image.data) {
-        std::cout << "Error: no se cargo la imagen." << std::endl;
-        return -1;
-    }
+}
 
-    cv::Mat imagen_resize;
-    if (arg_resolucion.compare("FHD") == 0 || arg_resolucion.compare("fhd") == 0){
-        cv::resize(image,imagen_resize,cv::Size(1920,1080));
-        cv::imwrite(arg_salida,imagen_resize);
-    }
-    if (arg_resolucion.compare("2K") == 0 || arg_resolucion.compare("2k") == 0){
-        cv::resize(image,imagen_resize,cv::Size(2048,1080));
-        cv::imwrite(arg_salida,imagen_resize);
-    }
-    if (arg_resolucion.compare("4K") == 0 || arg_resolucion.compare("4k") == 0){
-        cv::resize(image,imagen_resize,cv::Size(4096,2160));
-        cv::imwrite(arg_salida,imagen_resize);
-    }
-    if (arg_resolucion.compare("8K") == 0 || arg_resolucion.compare("8k") == 0){
-        cv::resize(image,imagen_resize,cv::Size(7680,4320));
-        cv::imwrite(arg_salida,imagen_resize);
-    }
-    
+int main(int argc, char const *argv[]) {
+    asociar_argumentos_imagen(argc,argv);
     cv::waitKey();
-	
     return 0;
 }
